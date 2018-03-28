fa55e33b410b1e73f24f0ce0319163ae90d00bfc
janschoenherr <jan@yootheme.com>
2018-03-28 14:03:25 +0200

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,55 +1,55 @@
 module.exports = {
-  "root":true,
-  "env": {
-      "es6": true,
-      "browser": true,
-      "commonjs": true,
-  },
-  "plugins": [
-      "vue"
-  ],
-  "extends": [
-      "eslint:recommended",
-      "plugin:vue/recommended"
-  ],
-  "parserOptions": {
-      "sourceType": "module"
-  },
-  "rules": {
-      "brace-style": ["error", "1tbs", {"allowSingleLine": true}],
-      "comma-style": "error",
-      "comma-spacing": "error",
-      "eqeqeq": ["off", "smart"],
-      "indent": "off",
-      "indent-legacy": ["error", 4, {"SwitchCase": 1}],
-      "key-spacing": "error",
-      "keyword-spacing": "error",
-      "linebreak-style": ["error", "unix"],
-      "no-var": "error",
-      "no-multi-spaces": "error",
-      "no-trailing-spaces": "error",
-      "no-lone-blocks": "error",
-      "no-extend-native": "error",
-      "no-unused-vars": ["error", {"vars": "local", "args": "none"}],
-      "no-empty": ["error", {"allowEmptyCatch": true}],
-      "no-duplicate-imports": "error",
-      "no-array-constructor": "error",
-      "no-multiple-empty-lines": "error",
-      "no-template-curly-in-string": "error",
-      "no-console": "off",
-      "object-curly-spacing": "error",
-      "prefer-const": "error",
-      "quotes": ["error", "single", {"avoidEscape": true}],
-      "semi": ["error", "always"],
-      "space-infix-ops": "error",
-      "space-unary-ops": "error",
-      "space-in-parens": "error",
-      "space-before-blocks": "error",
-      "template-curly-spacing": "error",
-      "vue/html-indent": ["error", 4],
-      "vue/html-self-closing": ["error", {"html": {"normal": "never"}}],
-      "vue/require-v-for-key": "off",
-      "vue/require-default-prop": "off",
-      "vue/max-attributes-per-line": "off"
-  }
+    "root": true,
+    "env": {
+        "es6": true,
+        "browser": true,
+        "commonjs": true,
+    },
+    "plugins": [
+        "vue"
+    ],
+    "extends": [
+        "eslint:recommended",
+        "plugin:vue/recommended"
+    ],
+    "parserOptions": {
+        "sourceType": "module"
+    },
+    "rules": {
+        "brace-style": ["error", "1tbs", {"allowSingleLine": true}],
+        "comma-spacing": "error",
+        "comma-style": "error",
+        "eqeqeq": ["off", "smart"],
+        "indent": "off",
+        "indent-legacy": ["error", 4, {"SwitchCase": 1}],
+        "key-spacing": "error",
+        "keyword-spacing": "error",
+        "linebreak-style": ["error", "unix"],
+        "no-array-constructor": "error",
+        "no-console": "off",
+        "no-duplicate-imports": "error",
+        "no-empty": ["error", {"allowEmptyCatch": true}],
+        "no-extend-native": "error",
+        "no-lone-blocks": "error",
+        "no-multi-spaces": "error",
+        "no-multiple-empty-lines": "error",
+        "no-template-curly-in-string": "error",
+        "no-trailing-spaces": "error",
+        "no-unused-vars": ["error", {"vars": "local", "args": "none"}],
+        "no-var": "error",
+        "object-curly-spacing": "error",
+        "prefer-const": "error",
+        "quotes": ["error", "single", {"avoidEscape": true}],
+        "semi": ["error", "always"],
+        "space-before-blocks": "error",
+        "space-in-parens": "error",
+        "space-infix-ops": "error",
+        "space-unary-ops": "error",
+        "template-curly-spacing": "error",
+        "vue/html-indent": ["error", 4],
+        "vue/html-self-closing": ["error", {"html": {"normal": "never"}}],
+        "vue/max-attributes-per-line": "off",
+        "vue/require-default-prop": "off",
+        "vue/require-v-for-key": "off"
+    }
 };
